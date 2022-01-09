2338a6e678496299f0bf058908a76712d26dd911
Daniel Correa <yo@danielgara.com>
2022-01-09 08:19:40 -0500

    imp: Chapter06

--- Chapter06/online-store/src/main.ts
+++ Chapter06/online-store/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as hbs from 'hbs';
+import * as hbsUtils from 'hbs-utils';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -10,6 +12,8 @@ async function bootstrap() {
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  hbs.registerPartials(join(__dirname, '..', 'views/layouts'));
+  hbsUtils(hbs).registerWatchedPartials(join(__dirname, '..', 'views/layouts'));
   app.setViewEngine('hbs');
 
   await app.listen(3000);
