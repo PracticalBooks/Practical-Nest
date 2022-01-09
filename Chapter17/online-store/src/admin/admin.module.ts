9f9bf6e07aaf117be0b914e73077ad9276d525ba
Daniel Correa <yo@danielgara.com>
2022-01-09 10:36:09 -0500

    imp: pre-chapter17

---
+++ Chapter17/online-store/src/admin/admin.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { AdminController } from './admin.controller';
+
+@Module({
+  controllers: [AdminController],
+})
+export class AdminModule {}
---
+++ Chapter17/online-store/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsService } from './models/products.service';
+import { Product } from './models/product.entity';
+import { AdminModule } from './admin/admin.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(),
+    TypeOrmModule.forFeature([Product]),
+    AdminModule,
+  ],
+  controllers: [AppController, ProductsController],
+  providers: [ProductsService],
+})
+export class AppModule {}
