4ba3bed0a198c3d645cfb48ae33d092d67a0a707
Daniel Correa <yo@danielgara.com>
2022-01-11 18:30:37 -0500

    imp: Chapter16

--- Chapter16/online-store/src/app.module.ts
+++ Chapter16/online-store/src/app.module.ts
@@ -4,11 +4,13 @@ import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsService } from './models/products.service';
 import { Product } from './models/product.entity';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     TypeOrmModule.forFeature([Product]),
+    AdminModule,
   ],
   controllers: [AppController, ProductsController],
   providers: [ProductsService],
