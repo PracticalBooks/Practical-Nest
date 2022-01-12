f987e9cff5c6c73d4c9bc0be8975d011a8844527
Daniel Correa <yo@danielgara.com>
2022-01-12 08:10:50 -0500

    imp: Chapter20

--- Chapter20/online-store/src/admin/admin.products.controller.ts
+++ Chapter20/online-store/src/admin/admin.products.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Render, Post, Body, Redirect,
-  UseInterceptors, UploadedFile } from '@nestjs/common';
+  UseInterceptors, UploadedFile, Param } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ProductsService } from '../models/products.service';
 import { Product } from '../models/product.entity';
@@ -29,5 +29,40 @@ export class AdminProductsController {
     newProduct.setPrice(body.price);
     newProduct.setImage(file.filename);
     await this.productsService.createOrUpdate(newProduct);
+  }
+
+  @Post('/:id')
+  @Redirect('/admin/products')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(id);
+  }
+
+  @Get('/:id')
+  @Render('admin/products/edit')
+  async edit(@Param('id') id: string) {
+    const viewData = [];
+    viewData['title'] = 'Admin Page - Edit Product - Online Store';
+    viewData['product'] = await this.productsService.findOne(id);
+    return {
+      viewData: viewData,
+    };
+  }
+
+  @Post('/:id/update')
+  @UseInterceptors(FileInterceptor('image', { dest: './public/uploads' }))
+  @Redirect('/admin/products')
+  async upload(
+    @Body() body, 
+    @UploadedFile() file: Express.Multer.File,
+    @Param('id') id: string
+  ) {
+    const product = await this.productsService.findOne(id);
+    product.setName(body.name);
+    product.setDescription(body.description);
+    product.setPrice(body.price);
+    if (file) {
+      product.setImage(file.filename);
+    }
+    await this.productsService.createOrUpdate(product);
   }
 }
--- Chapter20/online-store/src/models/products.service.ts
+++ Chapter20/online-store/src/models/products.service.ts
@@ -21,4 +21,8 @@ export class ProductsService {
   createOrUpdate(product: Product): Promise<Product> {
     return this.productsRepository.save(product);
   }
+
+  async remove(id: string): Promise<void> {
+    await this.productsRepository.delete(id);
+  }
 }
