1e57c7ca6d441d77b4aad0a4894e99e8446837fc
Daniel Correa <yo@danielgara.com>
2022-01-12 20:40:03 -0500

    imp: Chapter24

--- Chapter24/online-store/src/main.ts
+++ Chapter24/online-store/src/main.ts
@@ -30,6 +30,13 @@ async function bootstrap() {
     }
     next();
   });
+  app.use('/admin*', function (req, res, next) {
+    if (req.session.user && req.session.user.role == 'admin') {
+      next();
+    } else {
+      res.redirect('/');
+    }
+  });
 
   await app.listen(3000);
 }
